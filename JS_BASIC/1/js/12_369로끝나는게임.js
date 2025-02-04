4df3f9ec675cb636ec74d0d0faa9deffb3b4e188
Gilju Song <rlfwn@gmail.com>
2025-02-04 16:49:36 +0900

    js_if/else, function 활용하여 문제해결하기

---
+++ JS_BASIC/1/js/12_369로끝나는게임.js
@@ -0,0 +1,21 @@
+/**
+ * 📌 3의 배수에서 박수를 치는게 아니라 끝자리가 3,6,9로 끝나는 숫자라면 '박수'를 출력
+ */
+
+function game369(number) {
+  const lastnumber = number.toString().slice(-1); // 숫자의 마지막 자리 확인
+  if (lastnumber === '3' || lastDigit === '6' || lastDigit === '9') {
+    console.log('👏🏻');
+  } else {
+    console.log('통과');
+  }
+}
+
+// 함수 호출 예시
+game369(3);  
+game369(9);  
+game369(18); 
+game369(23); 
+game369(26); 
+game369(29); 
+game369(40);  
