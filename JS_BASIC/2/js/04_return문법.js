0822a967b1c5342b88acc39fa696a47d44b812bb
Gilju Song <rlfwn@gmail.com>
2025-02-07 14:54:05 +0900

    Feat:return 문제풀이

--- JS_BASIC/2/js/04_return문법.js
+++ JS_BASIC/2/js/04_return문법.js
@@ -9,3 +9,37 @@ function vat(a) {
   // 문자열로 반환된 반올림 값을 부동 소수점 숫자로 변환하여 반환합니다.
   return parseFloat(num);
 }
+
+// ✅ parseInt()는 정수로,
+// ✅ parseFloat()는 실수로 변환
+
+/**
+ *  함수에 분과 초를 차례로 파라미터로 입력하면 ms단위로 바꿔서 뱉어주는 함수를 만들어보기
+ */
+console.log(함수(1, 30));
+
+function 함수(min, sec) {
+  let result = (a * 60 + b) * 1000;
+  return result;
+}
+
+console.log(함수(2, 10));
+
+function 함수(min, sec) {
+  let result = (min * 60 + sec) * 1000;
+  return result;
+}
+
+/**
+ *  가격을 파라미터로 입력하면 10% 할인된 가격을 뱉는 함수 만들기
+ */
+console.log(함수(70, false)); // 함수 호출: 70의 10% 할인된 가격을 계산하고 false일 경우 추가 할인을 적용하지 않음
+
+function 함수(a, b) {
+  var result = a * 0.9; // 입력된 가격 a에 10% 할인을 적용
+  if (b == true) {
+    // b가 true일 경우 추가 할인을 적용
+    result = result - 1.5; // 추가로 1.5를 할인
+  }
+  return parseFloat(result.toFixed(2)); // 결과를 소수점 둘째 자리까지 반올림하여 반환
+}
